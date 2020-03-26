5d66ce7701121afbd0e07b7beb82274c8a1dffbe
ybakshi <35466030+ybakshi@users.noreply.github.com>
2020-03-26 06:48:13 -0700

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     $username = $url["user"];
     $password = $url["pass"];
     $db = substr($url["path"], 1);
-echo $url." ".$server." ".$username." ".$password." ".$db);
+echo $url." ".$server." ".$username." ".$password." ".$db;
     $conn = new mysqli($server, $username, $password, $db);
 
     echo $conn;
