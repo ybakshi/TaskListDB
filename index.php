7d3a1f333a0548bff9cc72737e5f4cdd2ffb5b23
ybakshi <35466030+ybakshi@users.noreply.github.com>
2020-03-26 10:09:54 -0700

    Update index.php

--- index.php
+++ index.php
@@ -76,15 +76,19 @@
 		</thead>
 
 		<tbody>
-			<?php $i = 1; while ($row = mysqli_fetch_array($tasks)) { ?>
-				<tr>
-					<td> <?php echo $i; ?> </td>
-					<td class="task"> <?php echo $row['task']; ?> </td>
-					<td class="delete"> 
-						<a href="index.php?del_task=<?php echo $row[$i] ?>">x</a> 
-					</td>
-				</tr>
-			<?php $i++; } ?>	
+			<?php 
+                // select all tasks if page is visited or refreshed
+                $tasks = mysqli_query($conn, "SELECT * FROM tasks");
+
+                $i = 1; while ($row = mysqli_fetch_array($tasks)) { ?>
+                    <tr>
+                        <td> <?php echo $i; ?> </td>
+                        <td class="task"> <?php echo $row['task']; ?> </td>
+                        <td class="delete"> 
+                            <a href="index.php?del_task=<?php echo $row['id'] ?>">x</a> 
+                        </td>
+                    </tr>
+		  <?php $i++; } ?>		
 		</tbody>
 	</table>
 
