6c5f2db44b13ce6389460aa02b84d331a3279ce6
ybakshi <35466030+ybakshi@users.noreply.github.com>
2020-03-26 10:39:52 -0700

    Update index.php

--- index.php
+++ index.php
@@ -86,7 +86,7 @@
                         <td> <?php echo $i; ?> </td>
                         <td class="task"> <?php echo $row['task']; ?> </td>
                         <td class="delete"> 
-                            <a href="index.php?del_task=\"<?php echo $row['task']; ?>\"">x</a>
+                            <a href="index.php?del_task='<?php echo $row['task']; ?>'">x</a> 
                         </td>
                     </tr>
 		  <?php $i++; } ?>		
