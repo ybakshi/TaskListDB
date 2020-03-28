cc909b2820a8b50ec1193d8bc05d720309ca4fc9
ybakshi <35466030+ybakshi@users.noreply.github.com>
2020-03-28 12:25:58 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,29 @@
 <?php 
 
     //Check for login first
-    if (strpos($_SERVER['HTTP_REFERER'], $_SERVER['PHP_SELF'],1) == false){
-        if (strcmp($_POST['userName'],$_POST['userName']) != 0) {
+//    if (strpos($_SERVER['HTTP_REFERER'], $_SERVER['PHP_SELF'],1) == false){
+//        if (isset($_POST['userName'])){
+//            if (strcmp($_POST['userName'],"admin") != 0) {
+//                header("Location: login.php");
+//                exit();
+//            }
+//        }else{
+//            header("Location: login.php");
+//            exit();
+//        }
+//    }else{
+//        
+//    }
+
+        if (isset($_POST['userName'])){
+            if (strcmp($_POST['userName'],"admin") != 0) {
+                header("Location: login.php");
+                exit();
+            }
+        }else{
             header("Location: login.php");
             exit();
         }
-    }
     
 	$errors = "";
 
