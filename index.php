311557b1d5b8edf60cfffb9c658b5856d768b4e3
ybakshi <35466030+ybakshi@users.noreply.github.com>
2020-03-31 08:05:28 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,37 +1,19 @@
 <?php 
-
-    //See if the user is valid
-    if (strcmp($_SESSION["user"], "auth") !== 0) {
-        header("Location: login.php");
-        exit();
-    }
+    session_start();
 
     $time = $_SERVER['REQUEST_TIME'];
 
     /**
     * for a 30 minute timeout, specified in seconds
     */
-    $timeout_duration = 1800;
-
-    /**
-    * Here we look for the user's LAST_ACTIVITY timestamp. If
-    * it's set and indicates our $timeout_duration has passed,
-    * blow away any previous $_SESSION data and start a new one.
-    */
-    if (isset($_SESSION['LAST_ACTIVITY']) && 
-       ($time - $_SESSION['LAST_ACTIVITY']) > $timeout_duration) {
-        session_unset();
-        session_destroy();
-        session_start();
+    $timeout_duration = 3600;
+    
+    //See if the user is valid
+    if (strcmp($_SESSION["user"], "auth") !== 0) {
+        header("Location: login.php");
+        exit();
     }
-
-    /**
-    * Finally, update LAST_ACTIVITY so that our timeout
-    * is based on it and not the user's login time.
-    */
-    $_SESSION['LAST_ACTIVITY'] = $time;
-    
-    
+        
 	$errors = "";
 
 	// connect to database heroku DB
